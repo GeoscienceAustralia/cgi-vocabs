224973bb9b1db5beccdd78f5d255987452027a51
jamiefeiss <jamie.feiss@gmail.com>
2021-08-31 16:00:50 +1000

    Updated vocabs for validation, added handling warnings in validate_vocabs

--- scripts/validate_vocabs.py
+++ scripts/validate_vocabs.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from pyshacl import validate
 import httpx
 
+WARNINGS_INVALID = False # Allows warnings to flag as invalid when true
+SHOW_WARNINGS = True
 
 def main():
     # get the validator
@@ -10,6 +12,7 @@ def main():
     assert r.status_code == 200
 
     # for all vocabs...
+    warning_vocabs = {} # format {vocab_filename: warning_msg}
     invalid_vocabs = {} # format {vocab_filename: error_msg}
     vocabs_dir = Path(__file__).parent.parent / "vocabularies"
     for f in vocabs_dir.glob("**/*"):
@@ -18,10 +21,22 @@ def main():
             try:
                 v = validate(str(f), shacl_graph=r.text, shacl_graph_format="ttl")
                 if not v[0]:
-                    invalid_vocabs[f.name] = v[2]
+                    if "Severity: sh:Violation" in v[2]:
+                        invalid_vocabs[f.name] = v[2]
+                    elif "Severity: sh:Warning" in v[2]:
+                        warning_vocabs[f.name] = v[2]
+
             # syntax errors crash the validate() function
             except Exception as e:
                 invalid_vocabs[f.name] = e
+
+    # check to see if we have any invalid vocabs
+    if len(warning_vocabs.keys()) > 0 and SHOW_WARNINGS:
+        print("Warning Vocabs:\n")
+        for vocab, warning in warning_vocabs.items():
+            print(f"- {vocab}:")
+            print(warning)
+            print("-----")
 
     # check to see if we have any invalid vocabs
     if len(invalid_vocabs.keys()) > 0:
@@ -31,6 +46,8 @@ def main():
             print(error)
             print("-----")
 
+    if WARNINGS_INVALID:
+        assert len(warning_vocabs.keys()) == 0, "Warning vocabs: {}".format(", ".join([str(x) for x in warning_vocabs.keys()]))
     assert len(invalid_vocabs.keys()) == 0, "Invalid vocabs: {}".format(", ".join([str(x) for x in invalid_vocabs.keys()]))
 
 
